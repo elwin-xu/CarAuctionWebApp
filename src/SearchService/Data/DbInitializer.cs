9c083fde0cf1f8ee8ae052eabe28489be4474710
Elwin <elwin.xu@velvix.com>
2024-01-05 11:14:07 +1100

    Adding Http communication to get the data

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -18,16 +18,16 @@ public class DbInitializer
 
         var count = await DB.CountAsync<Item>();
 
-        if (count == 0)
+        using var scope = app.Services.CreateScope();
+
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+        var items = await httpClient.GetItemsForSearchDb();
+
+        Console.WriteLine(items.Count + " returned from the Auction Service");
+
+        if (items.Count > 0)
         {
-            Console.WriteLine("No data - will attempt to seed");
-
-            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-
-            var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
-
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
-
             await DB.SaveAsync(items);
         }
     }
